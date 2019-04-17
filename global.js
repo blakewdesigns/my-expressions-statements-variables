ff048feaae42f70e9d2796b06c3f0acb8d744728
Blake Walker <blake.wdesigns@gmail.com>
2019-04-17 11:43:50 -0400

    Update global.js

--- global.js
+++ global.js
@@ -1,42 +1,45 @@
 
 // Welcome! Here's an example of a console logged response
-console.log(4 + 5)
+// console.log(4 + 5)
 
 /***********************/
 /* Part 1: Expressions */
 /***********************/
 
 //1.) Log an expression that evaluates to the sum of two integers
-// your code here 
-console.log('answer 1')
+let b = 5
+let c = 8
+console.log(b + c)
 
 // 2.) log an expression that evaluates to the difference of two integers
 // your code here 
-console.log('answer 2')
+console.log(c - b)
 
 // 3.) Log an expression that concatenates the two strings "I'm writing" and "my first JavaScript code"
-// your code here 
-console.log('answer 3')
+let a = 'I\'m writing'
+let d = 'my first JavaScript code.'
+console.log(a + d)
 
 // 4.) Log an expression that evaluates to true or false using two numbers that are greater or less than eachother
-// your code here 
-console.log('answer 4')
+let e = 10
+let f = 4
+console.log(e > f)
 
 /**********************/
 /* Part 2: Statements */
 /**********************/
 
 // 5.) Assign a variable and console log the declaration
-// your code here 
-console.log('answer 5')
+g = 'dog' 
+console.log(g)
 
 // 6.) Console log a variable assigned to the sum of two numbers
-// your code here
-console.log('answer 6')
+var x = 15 + 10
+console.log(x)
 
 // 7.) Uncomment this if else statement and add a logical expression as the condition
-//if (/*your code here*/) {
-//    console.log('It was!')
-//} else {
-//    console.log('It was not..')
-//}
+if (15 > 2) {
+   console.log('It was!')
+} else {
+   console.log('It was not..')
+}
